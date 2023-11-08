1d12057abf1cefb513470116806194a86160dd34
QuantumRuler <bvedant8@gmail.com>
2023-11-09 00:01:01 +0530

    Basic in done

--- tut-1/main.go
+++ tut-1/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello World")
@@ -11,9 +13,17 @@ func main() {
 	//Phase 2
 
 	var userName string
-
 	userName = "Vedant"
-
 	fmt.Println(userName)
 
+	//Phase 3 Getting types
+
+	fmt.Printf(" The type of the variable %v is %T \n", userName, userName)
+
+	//Phase 4
+	var userid string
+	fmt.Println("Enter your userid")
+	fmt.Scan(&userid)
+
+	fmt.Printf("We user has been set to %v \n", userid)
 }
