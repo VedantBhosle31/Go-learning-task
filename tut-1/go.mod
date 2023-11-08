2c4ab8846459be1576e8e1d49015784c892afcab
QuantumRuler <bvedant8@gmail.com>
2023-11-08 23:44:44 +0530

    Added till Tut1 phase 2

---
+++ tut-1/go.mod
@@ -0,0 +1,3 @@
+module tut-1
+
+go 1.21.4
---
+++ tut-1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	// Phase !
+	var ConferenceName = "Go confrence"
+	fmt.Printf("Welcome to %v \n", ConferenceName)
+
+	//Phase 2
+
+	var userName string
+
+	userName = "Vedant"
+
+	fmt.Println(userName)
+
+}
