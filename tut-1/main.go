3b407cec5eb834c017bc6f0ba4f24a1d385ce17d
QuantumRuler <bvedant8@gmail.com>
2023-11-09 00:33:27 +0530

    Addded Arrays and Slices

--- tut-1/main.go
+++ tut-1/main.go
@@ -5,25 +5,35 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello World")
-	// Phase !
-	var ConferenceName = "Go confrence"
-	fmt.Printf("Welcome to %v \n", ConferenceName)
+	fmt.Println("Welcome to the Go Confrence")
+	const totalTickets int = 50
+	var remainingTickets uint = 50
+	fmt.Printf("We have only %v tickets left.\nPlease Register to Confirm your booking", remainingTickets)
 
-	//Phase 2
+	var firstName string
+	var lastName string
+	var email string
+	var requestedTickets uint
 
-	var userName string
-	userName = "Vedant"
-	fmt.Println(userName)
+	fmt.Printf("Enter your First Name ")
+	fmt.Scan(&firstName)
+	fmt.Printf("Enter your Last Name ")
+	fmt.Scan(&lastName)
+	fmt.Printf("Enter your email ")
+	fmt.Scan(&email)
+	fmt.Printf("Enter how mant tickets your want ")
+	fmt.Scan(&requestedTickets)
 
-	//Phase 3 Getting types
+	remainingTickets = remainingTickets - requestedTickets
 
-	fmt.Printf(" The type of the variable %v is %T \n", userName, userName)
+	fmt.Printf("Thank you %v %v for booking % v Tickets./n", firstName, lastName, remainingTickets)
+	fmt.Printf("You will recevie a confirmation mail on %v \n", email)
+	fmt.Printf("Number of remaining Tickets is %v\n", remainingTickets)
 
-	//Phase 4
-	var userid string
-	fmt.Println("Enter your userid")
-	fmt.Scan(&userid)
+	var allTheAttendes []string
+	allTheAttendes = append(allTheAttendes, firstName+" "+lastName)
 
-	fmt.Printf("We user has been set to %v \n", userid)
+	fmt.Printf("The List of all the attendees is %v.\n", allTheAttendes)
+	fmt.Printf("Number people who booked the tickets is%v.\n", len(allTheAttendes))
+
 }
