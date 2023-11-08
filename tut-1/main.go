4f1ea11b82f832cafb211846d5c9bc64359c7eee
QuantumRuler <bvedant8@gmail.com>
2023-11-09 00:45:21 +0530

    Added loops to the application'

--- tut-1/main.go
+++ tut-1/main.go
@@ -2,38 +2,50 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to the Go Confrence")
-	const totalTickets int = 50
-	var remainingTickets uint = 50
-	fmt.Printf("We have only %v tickets left.\nPlease Register to Confirm your booking", remainingTickets)
 
-	var firstName string
-	var lastName string
-	var email string
-	var requestedTickets uint
+	for {
 
-	fmt.Printf("Enter your First Name ")
-	fmt.Scan(&firstName)
-	fmt.Printf("Enter your Last Name ")
-	fmt.Scan(&lastName)
-	fmt.Printf("Enter your email ")
-	fmt.Scan(&email)
-	fmt.Printf("Enter how mant tickets your want ")
-	fmt.Scan(&requestedTickets)
+		fmt.Println("Welcome to the Go Confrence")
+		const totalTickets int = 50
+		var remainingTickets uint = 50
+		fmt.Printf("We have only %v tickets left.\nPlease Register to Confirm your booking", remainingTickets)
 
-	remainingTickets = remainingTickets - requestedTickets
+		var firstName string
+		var lastName string
+		var email string
+		var requestedTickets uint
 
-	fmt.Printf("Thank you %v %v for booking % v Tickets./n", firstName, lastName, remainingTickets)
-	fmt.Printf("You will recevie a confirmation mail on %v \n", email)
-	fmt.Printf("Number of remaining Tickets is %v\n", remainingTickets)
+		fmt.Printf("Enter your First Name ")
+		fmt.Scan(&firstName)
+		fmt.Printf("Enter your Last Name ")
+		fmt.Scan(&lastName)
+		fmt.Printf("Enter your email ")
+		fmt.Scan(&email)
+		fmt.Printf("Enter how mant tickets your want ")
+		fmt.Scan(&requestedTickets)
 
-	var allTheAttendes []string
-	allTheAttendes = append(allTheAttendes, firstName+" "+lastName)
+		remainingTickets = remainingTickets - requestedTickets
 
-	fmt.Printf("The List of all the attendees is %v.\n", allTheAttendes)
-	fmt.Printf("Number people who booked the tickets is%v.\n", len(allTheAttendes))
+		fmt.Printf("Thank you %v %v for booking % v Tickets./n", firstName, lastName, remainingTickets)
+		fmt.Printf("You will recevie a confirmation mail on %v \n", email)
+		fmt.Printf("Number of remaining Tickets is %v\n", remainingTickets)
+
+		var allTheAttendes []string
+		allTheAttendes = append(allTheAttendes, firstName+" "+lastName)
+		firstNames := []string{}
+
+		for _, attendee := range allTheAttendes {
+			var name = strings.Fields(attendee)
+			firstNames = append(firstNames, name[0])
+		}
+
+		fmt.Printf("The List of all the attendees is %v.\n", firstNames)
+		fmt.Printf("Number people who booked the tickets is%v.\n", len(firstNames))
+
+	}
 
 }
